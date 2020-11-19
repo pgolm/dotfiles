780f8b5b22cfd97799ce8cfd07cd82bb01fa080b
Peter Golm <golm.peter@gmail.com>
2020-11-20 00:07:27 +0100

    fix typo

--- run_once_install-fisher.sh
+++ run_once_install-fisher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! [ -x "$(command -v fish)" ]; then
+if [ -x "$(command -v fish)" ]; then
     curl -sL git.io/fisher | source && fisher install jorgebucaran/fisher
 
     fisher install rafaelrinaldi/pure
