b48a66df3910e31dad344ad944e3881d3a9c99a8
Peter Golm <golm.peter@gmail.com>
2020-11-25 00:33:26 +0100

    wip

--- dot_config/private_fish/conf.d/asdf.fish
+++ dot_config/private_fish/conf.d/asdf.fish
@@ -1,5 +1,9 @@
-if test -e /usr/local/opt/asdf/asdf.fish
-    source /usr/local/opt/asdf/asdf.fish
+set ASDF_SOURCE $HOME/.asdf/asdf.fish
+if [ ! -e $ASDF_SOURCE ]
+    set ASDF_SOURCE /usr/local/opt/asdf/asdf.fish
+end
 
+if test -e $ASDF_SOURCE
+    source $ASDF_SOURCE
     eval (asdf exec direnv hook fish)
 end
