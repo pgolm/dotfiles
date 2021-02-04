9bc4f54af0d316168986167a4168ba7276427a16
Peter Golm <peter.golm@visable.com>
2021-02-04 09:13:13 +0100

    wip

--- dot_config/private_fish/conf.d/asdf.fish
+++ dot_config/private_fish/conf.d/asdf.fish
@@ -1,7 +1,7 @@
-set ASDF_SOURCE $HOME/.asdf/asdf.fish
-if [ ! -e $ASDF_SOURCE ]
+# set ASDF_SOURCE $HOME/.asdf/asdf.fish
+# if [ ! -e $ASDF_SOURCE ]
     set ASDF_SOURCE /usr/local/opt/asdf/asdf.fish
-end
+# end
 
 if test -e $ASDF_SOURCE
     source $ASDF_SOURCE
