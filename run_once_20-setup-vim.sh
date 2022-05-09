c6e53a5925f28a37314779ac1cbccb20504ffa98
Peter Golm <golm.peter@gmail.com>
2022-05-10 00:29:48 +0200

    wip

--- run_once_20-setup-vim.sh
+++ run_once_20-setup-vim.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [ "$(command -v vim)" ]; then
-vim +PluginInstall +qall
+    vim +PluginInstall +qall
 fi
