b64982f22bbccd5a2aabe0e166fd7d131a001a09
Peter Golm <peter.golm@visable.com>
2022-08-09 10:33:46 +0200

    fix: improve setup on linux systems

--- run_once_20-setup-fish.sh
+++ run_once_20-setup-fish.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env fish
 
-if [ -n $fisher_version ]
-    curl -sL git.io/fisher | source && fisher update
-else
-    fisher update
-end
+curl -sL git.io/fisher | source && fisher update
