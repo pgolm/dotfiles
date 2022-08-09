0f0f642d50f2af80a49889ba1f7491fbc60df591
Peter Golm <peter.golm@visable.com>
2022-08-09 11:52:14 +0200

    wip

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,9 @@
 set -g LC_ALL en_US.UTF-8
 
-set -g EDITOR vim
-
 if test -d $HOME/.local/bin
     set -x PATH $HOME/.local/bin $PATH
 end
 
-if status --is-interactive
-    if command -s starship > /dev/null
-        starship init fish | source
-    end
+if command -s starship > /dev/null
+    starship init fish | source
 end
--- run_after_20-setup-fish.sh
+++ run_after_20-setup-fish.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env -S fish -d 3
 
-set -U pipefail
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && fisher update
+curl -sL https://git.io/fisher | source # && fisher install jorgebucaran/fisher && fisher update
