bf865c667cc92f65d5d34bbde615965516c619ba
Peter Golm <peter.golm@visable.com>
2022-08-09 11:53:23 +0200

    wip

--- run_after_20-setup-fish.sh
+++ run_after_20-setup-fish.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env -S fish -d 3
 
-curl -sL https://git.io/fisher | source # && fisher install jorgebucaran/fisher && fisher update
+curl -sL https://git.io/fisher | source
+fisher install jorgebucaran/fisher
+fisher update
