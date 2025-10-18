ed97bea42b37723443f896664002c3a905c166fb
Peter Golm <golm.peter@gmail.com>
2025-10-18 16:31:56 +0200

    Simplify shell init checks and add Docker completions to fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,31 +1,33 @@
-# shellcheck shell=fish
-
-# Initialize homebrew paths
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/sbin
-
 set -g fish_greeting # Unset welcome message
 set -g EDITOR nvim
 
 alias vim='nvim'
 alias vi='nvim'
 
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+
 # Initialize Starship prompt if available
-if command -s starship > /dev/null
+if command -s starship >/dev/null
     starship init fish | source
 end
 
 # Initialize Mise if available
-if command -s mise > /dev/null
+if command -s mise >/dev/null
     mise activate fish | source
 end
 
 # Initialize Atuin if available
-if command -s atuin > /dev/null
+if command -s atuin >/dev/null
     atuin init fish --disable-up-arrow | source
 end
 
 # Initialize Atuin if available
-if command -s zoxide > /dev/null
+if command -s zoxide >/dev/null
     zoxide init fish | source
+end
+
+# Initialize Docker completions if available
+if command -s docker >/dev/null
+    docker completion fish | source
 end
